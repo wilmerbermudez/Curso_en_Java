9368ae2806ac5f0ad079c3dc5444752cd9ac1e64
Hakuna Matata <wfbermudez@utp.edu.co>
2020-01-14 17:47:38 -0500

    Sentancias de control If/Else/If/Else

---
+++ EjemploIfElseIfElse/src/sentenciascontrol/EjemploIfElseIfElse.java
@@ -0,0 +1,23 @@
+package sentenciascontrol;
+
+public class EjemploIfElseIfElse {
+
+    public static void main(String[] args) {
+        
+        int x = 10;
+        
+        if(x == 10){
+            System.out.println("X igual a 10\n");
+        }
+        else if(x == 20){
+            System.out.println("X igual a 20\n");
+        }
+        else if(x == 30){
+            System.out.println("X igual a 30\n");
+        }
+        else{
+            System.out.println("X no es igual ni a 10, ni a 20, ni a 30\n");
+        }
+    }
+    
+}
