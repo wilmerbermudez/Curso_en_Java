b5da109033745c31ac06d582c57d29867ff5e92e
Hakuna Matata <wfbermudez@utp.edu.co>
2020-01-14 17:11:07 -0500

    Sentencias de control If/Else Curso Java

--- EjemploIfElse/src/sentenciascontrol/EjemploIfElse.java
+++ EjemploIfElse/src/sentenciascontrol/EjemploIfElse.java
@@ -3,7 +3,14 @@ package sentenciascontrol;
 public class EjemploIfElse {
 
     public static void main(String[] args) {
-
+        int x = 30;
+        
+        if(x < 20){
+            System.out.println("X menor que 20\n");
+        }
+        else{
+            System.out.println("X mayor que 20\n");
+        }
     }
     
 }
