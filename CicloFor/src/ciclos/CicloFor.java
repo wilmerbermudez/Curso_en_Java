97e4c18244fadede98d74b0dec27ff3e7b4648d2
Hakuna Matata <wfbermudez@utp.edu.co>
2020-01-15 02:42:29 -0500

    Ciclo For Curso Java

---
+++ CicloFor/src/ciclos/CicloFor.java
@@ -0,0 +1,12 @@
+package ciclos;
+
+public class CicloFor {
+
+    public static void main(String[] args) {
+        int limite = 10;
+        for (int contador = 0; contador < limite; contador++) {
+            System.out.println("Contador = " + contador); 
+        }
+    }
+    
+}
