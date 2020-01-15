4c306845bb645a57d15dd80568d14b853068f962
Hakuna Matata <wfbermudez@utp.edu.co>
2020-01-14 22:05:57 -0500

    Sentencias de control Switch

---
+++ CalculaMesSwitch/src/sentenciascontrol/CalculaMesSwitch.java
@@ -0,0 +1,28 @@
+package sentenciascontrol;
+
+public class CalculaMesSwitch {
+
+    public static void main(String[] args) {
+        int mes = 20;
+        String estacion;
+        
+        switch(mes){
+            case 1: case 2: case 12:
+                estacion = "Invierno";
+                break;
+            case 3: case 4: case 5:
+                estacion = "Primavera";
+                break;
+            case 6: case 7: case 8:
+                estacion = "Verano";
+                break;
+            case 9: case 10: case 11:
+                estacion = "Otoño";
+                break;
+            default:
+                estacion = "Mes Incorrecto";
+        }
+        System.out.println("La estacion para el mes " + mes + " es: " + estacion);
+    }
+    
+}
