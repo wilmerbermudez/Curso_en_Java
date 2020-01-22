93e401594ca63476d6e9ffbcb821b2661ce0c95b
Hakuna Matata <wfbermudez@utp.edu.co>
2020-01-22 17:00:28 -0500

    Ejercicion return Tipos Primitivos curso Java

---
+++ PalabraReturn/src/palabrareturn/PalabraReturn.java
@@ -0,0 +1,17 @@
+package palabrareturn;
+
+public class PalabraReturn {
+
+    public static void main(String[] args) {
+        int resultado = sumar(3, 2);
+        System.out.println("Resultado: " + resultado);
+    }
+    
+    public static int sumar(int a, int b){
+        if (a == 0 && b == 0) {
+            System.out.println("Puede proporcionar valores distintos a cero");
+            return 0;
+        }
+        return a + b;
+    }
+}
